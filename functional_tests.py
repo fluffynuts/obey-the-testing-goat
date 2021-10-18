cc78a219c20c77a70156c0cd3d02a6541dd90ea6
Davyd McColl <davydm@gmail.com>
2021-10-18 09:28:27 +0200

    :construction: testing base

--- functional_tests.py
+++ functional_tests.py
@@ -1,26 +1,54 @@
-import os
 import subprocess
-
 from selenium import webdriver
-from pytest import fixture, mark
+from unittest import TestCase, main as test_main
 
 
-class TestFunctional:
-    @fixture
-    def browser(self):
-        browser = webdriver.Firefox()
-        yield browser
-        browser.close()
+class TestFunctional(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-    @fixture
+    def tearDown(self):
+        try:
+            self.browser.quit()
+        except:
+            pass
+
     def server(self):
         cmd = subprocess.Popen("virtualenv/scripts/python manage.py runserver")
         yield cmd
         cmd.kill()
 
-    def test_should_start_basic_django_site(self, browser, server):
+    def test_should_be_able_to_start_a_list_and_retrieve_it_later(self):
         # arrange
+        browser = self.browser
         # act
+        # user visits the site's home page
         browser.get("http://localhost:8000")
         # assert
-        assert "Django" in browser.title
+
+        # she should see the page title
+        self.assertIn("To-Do", browser.title)
+
+        # she should be asked to enter a to-do item immediately
+
+        # she types "Buy peacock feathers" into a text box (she makes fishing lures)
+
+        # she hits enter, the page updates and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # there is still a text box to add another item
+        # she adds "Use peacock feathers to make a fly"
+
+        # the page updates again and now both items are in the list
+
+        # the site "remembers the list" via url parameters
+        # there should be some text explaining this
+        # - she visits that url - her to-do list is still there
+
+        # she quits
+        # technically, we could leave this out as the fixture cleans up, but it's part of the story (:
+        browser.quit()
+
+
+if __name__ == "__main__":
+    test_main()

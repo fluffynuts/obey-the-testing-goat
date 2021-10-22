68840218bd6e7bea6a72b9ba66856cbdf935d664
Davyd McColl <davydm@gmail.com>
2021-10-22 15:48:59 +0200

    :white_check_mark: flesh out more of the functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,10 @@
 import subprocess
+import time
+
 from selenium import webdriver
 from unittest import TestCase, main as test_main
+
+from selenium.webdriver.common.keys import Keys
 
 
 class TestFunctional(TestCase):
@@ -26,13 +30,32 @@ class TestFunctional(TestCase):
 
         # she should see the page title
         self.assertIn("To-Do", browser.title)
+        header = browser.find_element_by_tag_name("h1")
+        self.assertIn("To-Do", header.text)
 
         # she should be asked to enter a to-do item immediately
+        input_box = browser.find_element_by_id("new_item")
+        self.assertEqual(
+            input_box.get_attribute("placeholder"),
+            "Enter a to-do item"
+        )
 
         # she types "Buy peacock feathers" into a text box (she makes fishing lures)
+        input_box.send_keys("Buy peacock feathers")
 
         # she hits enter, the page updates and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        input_box.send_keys(Keys.ENTER)
+        # FIXME: this will cause flaky tests, but we have to wait for the
+        # page to reload
+        time.sleep(1)
+
+        table = browser.find_element_by_id("list_table")
+        self.assertIsNotNone(table)
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == "1: Buy peacock feathers" for row in rows)
+        )
 
         # there is still a text box to add another item
         # she adds "Use peacock feathers to make a fly"
